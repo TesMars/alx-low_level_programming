967710030a8791ba697513e02f1854a6ccdd2e69
TesMars <tesfayemarshall@outlook.com>
2023-06-19 14:58:45 -0700

    size of various types commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * Return: 0 (Success)
  */
 int main(void)
-{
+i{
 	char a;
 	int b;
 	long int c;
@@ -16,5 +16,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long init: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
-return 0;
+return (0);
 }
