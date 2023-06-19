c20331cd8d7ffc8d850f45eaec49817c5e22cf71
TesMars <tesfayemarshall@outlook.com>
2023-06-19 12:28:04 -0700

    puts function commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * main - This program prints a line with puts function
- * Return: 0 (Done.)
+ * Return: 0 (Success)
  */
-int main(void) {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
