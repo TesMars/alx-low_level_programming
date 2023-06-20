ac26abd8b1d519de5da6b7b21791b6abed311dfa
TesMars <tesfayemarshall@outlook.com>
2023-06-19 18:20:35 -0700

    6-size.c commit message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - This script prints the size of various types on the computer
- * Return: 0 (Success)
+ * Return 0 (Success)
  */
 int main(void)
-i{
+{
 	char a;
 	int b;
 	long int c;
@@ -15,6 +15,6 @@ printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long init: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
