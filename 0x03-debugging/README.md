84f0bc571715431957fa2aa77dc569c1d191c4af
TesMars <tesfayemarshall@outlook.com>
2023-06-23 12:45:27 -0700

    changes

--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-This is a readme file 
+This is a readme for alx-low_level_pro
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- *main - Determine a positive, negative or zero number
-*0 : Is the number to be checked
-*Return: 0 on success
-*/
+ * main - Determine a positive , negative or zero.
+ * 0 : is the number to be checked
+ * Return: 0 on success
+ */
 
 void positive_or_negative(int i)
 {
-	if (i < 0)
-	{
-		printf("%d is %s\n", i, "negative");
-	}
-	else if (i > 0)
-	{
-	printf("%d is %s\n", i, "positive");
-	}
-	else
-	{
-		printf("%d is %s\n", i, "zero");
-	}
-	return;
+if (i < 0)
+{
+printf("%d is %s\n", i, "negative");
+}
+else if (i > 0)
+{
+printf("%d is %s\n", i, "positive");
+}
+else
+{
+printf("%d is %s\n", i, "zero");
+}
+return;
 }
