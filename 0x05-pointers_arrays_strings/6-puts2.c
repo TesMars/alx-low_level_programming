5800d9064133e3f6b4fa276e0a9357d32fc1cbde
TesMars <tesfayemarshall@outlook.com>
2023-06-27 12:59:19 -0700

    final comit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * puts2 - function should print only one character out of two 
- * starting with the first one 
- * @str: input 
- * Return: print 
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
  */
 
 void puts2(char *str)
@@ -13,13 +13,12 @@ void puts2(char *str)
 	int t = 0;
 	char *y = str;
 	int o;
-
 	while (*y != '\0')
 	{
 		y++;
 		longi++;
 	}
-	t = longi -1;
+	t = longi - 1;
 	for (0 = 0 ; o <= t ; o++)
 	{
 		if (o % 2 == 0)
